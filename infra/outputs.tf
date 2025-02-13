d219b23430f592b54b92ef5520e3da5d84183cc5
Pedro Reis <jpdro.reis@gmail.com>
2025-02-13 00:36:41 +0000

    fix: outputs file fixed

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,33 +5,3 @@ output "target_group_arn" {
 output "nlb_dns_name" {
   value = format("http://%s", aws_lb.this.dns_name)
 }
------
-
-26. sg.tf dentro da diretório de infra
-27. colar o conteúdo abaixo dentro do arquivo:
-
------
-# Crie um grupo de segurança
-resource "aws_security_group" "allow_inbound" {
-  name        = format("%s-sg", var.cluster_name)
-  vpc_id      = var.vpc_id
-  description = "Allow inbound traffic"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = format("%s-sg", var.cluster_name)
-  }
-}
